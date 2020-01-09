07ce5a4855998d6ecf6906050c3933da4ec1d335
Sugam Devare <sugamxp@gmail.com>
2020-01-09 10:21:01 +0530

    get latest articles endpoint added

--- incread_backend/users/views.py
+++ incread_backend/users/views.py
@@ -5,6 +5,11 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import UserArticle,CustomUser
 from .serializers import UserArticleSerializer,UserSerializer
+import requests
+from .models import UserArticle
+from articles.models import Article, Publisher
+import tldextract
+from rest_framework import viewsets, status as http_status
 
 class UserArticleViewSet(viewsets.ModelViewSet):
     queryset = UserArticle.objects.all()
@@ -63,3 +68,74 @@ class UserViewSet(viewsets.ModelViewSet):
         
         # response = {'message':'Success'}
         return Response(response, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['GET'])
+    def get_latest_articles(self, request,pk=None):
+        user = CustomUser.objects.filter(id=pk)[0]
+        access_token = user.access_token
+        last_user_article = UserArticle.objects.filter(user_fk = user).order_by('-time_added_pocket')[0]
+        last_timestamp = last_user_article.time_added_pocket
+        print(last_timestamp)
+        url = f'https://getpocket.com/v3/get?consumer_key=85449-9131725c86119d6dca1cbd8a&access_token={access_token}&\
+                 detailType=detailed&state=unread&sort=newest&since={last_timestamp}'
+
+        res = requests.get(url).json()
+        articles = res['list']
+
+        for i, article_id in enumerate(articles):
+            article = articles[article_id]
+            print(article_id)
+
+            #Publisher Model
+            title = article.get('domain_metadata',{}).get('name')
+            logo = article.get('domain_metadata',{}).get('logo')
+            publisher_url = tldextract.extract(article.get('resolved_url')).registered_domain
+
+            publisher, status =  Publisher.objects.get_or_create(title=title, url=publisher_url, logo=logo)
+            print("P =>",status)
+
+            #Article Model
+            url = article.get('resolved_url')
+            title = article.get('resolved_title')
+            excerpt = article.get('excerpt')
+            is_article = True if article.get('is_article')=="1" else False
+            has_video = True if article.get('has_video')=="1" else False
+            has_image = True if article.get('has_image')=="1" else False
+            word_count = article.get('word_count')
+            lang = article.get('lang')
+            time_to_read = article.get('time_to_read')
+            top_image_url = article.get('top_image_url')
+            listen_duration_estimate = article.get('listen_duration_estimate')
+
+            curr_article,status  = Article.objects.get_or_create(item_id=article_id, 
+                                        url=url, 
+                                        title=title, 
+                                        excerpt=excerpt,
+                                        is_article=is_article, 
+                                        has_video=has_video, 
+                                        has_image=has_image, 
+                                        word_count=word_count, 
+                                        lang=lang, 
+                                        time_to_read=time_to_read,
+                                        top_image_url=top_image_url, 
+                                        listen_duration_estimate=listen_duration_estimate, 
+                                        publisher_fk=publisher)
+            print("A =>", status)
+
+            #User Article
+            time_added_pocket = int(article.get('time_added', '0'))
+            time_updated_pocket =  int(article.get('time_updated', '0'))
+            time_read_pocket =  int(article.get('time_read','0'))
+            time_favorited_pocket =   int(article.get('time_favorited','0'))
+            status = int(article.get('status','0'))
+            UserArticle.objects.get_or_create(user_fk=user, 
+                                            article_fk=curr_article, 
+                                            publisher_fk = publisher,
+                                            time_added_pocket=time_added_pocket,
+                                            time_updated_pocket = time_updated_pocket,
+                                            time_read_pocket = time_read_pocket,
+                                            time_favorited_pocket =time_favorited_pocket,
+                                            status=status)
+
+        response = {'message' :'Success'}
+        return Response(response, status=http_status.HTTP_200_OK)
